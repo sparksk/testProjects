c2072892d449e66e775b258500943bb8315f6d21
Kevin Sparks <sparksk16@gmail.com>
2014-11-17 16:35:04 -0500

    A python code to create csv files

---
+++ csvCreation.py
@@ -0,0 +1,15 @@
+##Path location where new csv file will be stored
+csvPath = 'C:/Users/Sparks/Desktop'
+
+object1 = 'test1'
+object2 = 'test2'
+object3 = 'test3'
+
+##Opens the csv file to write to (can change 'w' to 'a' to append to)
+with open(csvPath+'/test.csv', "w") as DataTable:
+    ##Defines the number of columns and the header of each column
+    toWrite = "{0},{1},{2}\n".format('column x','column y','column z')
+    ##add more data
+    toWrite += "{0},{1},{2}\n".format(object1, object2, object3)
+    ##writes the additions to the csv file
+    DataTable.write(toWrite)
