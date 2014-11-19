7ab4b01dddb3f82ea0e0f35474ab34a39e4b4437
Kevin Sparks <sparksk16@gmail.com>
2014-11-19 11:51:23 -0500

    removed +1 in 'len(A)+1' when defining for loop

--- efficencyProblem.py
+++ efficencyProblem.py
@@ -5,17 +5,13 @@ A = [2, 4, -7, 3, -1, 2 , 6, 3, 2, -10, -3, 1, 2]
 
 # o(n^3)
 total = 0
-for i in range(0,len(A)+1):
-    for j in range(0,len(A)+1):
+for i in range(0,len(A)):
+    for j in range(0,len(A)):
         if sum(A[i:j]) >= total:
             total = sum(A[i:j])
 print total
 
 # o(n^2)
-total = 0
-test = 0
-for i in range(0,len(A)+1):
-    if test <= total:
-        test = A[i]
+
 
 # o(n)
