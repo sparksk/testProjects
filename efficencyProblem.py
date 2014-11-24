472ce96ff7177480e6e425d556de23dedb2b40e9
Kevin Sparks <sparksk16@gmail.com>
2014-11-23 23:28:25 -0500

    began new section on large list generation

--- efficencyProblem.py
+++ efficencyProblem.py
@@ -40,3 +40,41 @@ truepeakindex = indexpeak[peak.index(max(peak))]
 print truepeakindex
 # print trough
 print indextrough
+
+
+#########################
+import random
+# Not there yet... returns a list of positive floats
+B = [random.random() for _ in xrange(100000)]
+
+######### o(n^2) #########
+# total = 0
+# for i in range(0,len(B)):
+#     for j in range(0,len(B)):
+#         if sum(B[i:j]) >= total:
+#             total = sum(B[i:j])
+# print total
+
+######### o(n) #########
+# Greedy algorithm
+# total = 0
+# peak = []
+# trough = []
+# indexpeak = []
+# indextrough = []
+# for i in range(0,len(B)):
+#     if B[i] < 0:
+#         peak.extend([total])
+#         indexpeak.extend([i-1])
+#     total = total + B[i]
+#     if total < 0:
+#         trough.extend([total])
+#         # only marks a trough when total falls below 0
+#         indextrough.extend([i+1])
+#         total = 0
+# # print peak
+# # print indexpeak
+# truepeakindex = indexpeak[peak.index(max(peak))]
+# print truepeakindex
+# # print trough
+# print indextrough
