ed81c02aeae0acc248abd4c5647f39c70f87edea
Kevin Sparks <sparksk16@gmail.com>
2014-12-04 16:08:01 -0500

    Major updates to svm script

--- jonathanSVM.R
+++ jonathanSVM.R
@@ -23,100 +23,153 @@ data$hue_stdev <- as.numeric(data$hue_stdev)
 #data$Code <- as.factor(data$Code)
 
 
+
+
+
+
+
+
 ## Open Water - Class11
-dataOW <- data[data$Code == "Class11", ]
+dataOW <- data[data$Code == "Class11", 1:8]
+dataOW$Code <- "Class11"
 otherOW <- data[data$Code != "Class11", 1:8]
 otherOW$Code <- "Other"
 dataOW <- rbind(dataOW, otherOW)
 
-
 # Training data
 TrainingDataOW <- dataOW[1:as.integer(nrow(dataOW[dataOW$Code == "Class11", ]) * 0.80), ]
 TrainingDataOWOther <- dataOW[nrow(dataOW[dataOW$Code == "Class11", ])+1:as.integer(nrow(dataOW[dataOW$Code == "Other", ]) * 0.80), ]
-
 Trainingset <- rbind(TrainingDataOW, TrainingDataOWOther)
-
 
 # Testing data
 TestStartOW <- nrow(TrainingDataOW)+1 
 TestFinishOW <- nrow(dataOW[dataOW$Code == "Class11", ])
 TestingDataOW <- dataOW[TestStartOW:TestFinishOW, ]
-
 TestStartOther <- nrow(dataOW[dataOW$Code == "Class11", ])+nrow(TrainingDataOWOther)+1
 TestFinishOther <- nrow(dataOW)
 TestingDataOther <- dataOW[TestStartOther:TestFinishOther, ]
-
 Testingset <- rbind(TestingDataOW, TestingDataOther)
 
-
-
-
+# SVM 
 OWsvm <- svm(Code ~ brightness_median + brightness_stdev + saturation_median + saturation_stdev + hue_median + hue_stdev, 
 	kernel = "linear", type = "C-classification", data=Trainingset)
 summary(OWsvm)
 model.svm.test <- predict(OWsvm, Testingset[3:8], type="class")
+summary(model.svm.test)
 
 
 
 
 
-# Define how many folds for cross validation
-k <- 3
-# sample from 1 to k, nrow times (the number of observations in the data)
-dataOW$id <- sample(1:k, nrow(dataOW), replace = TRUE)
-list <- 1:k
-
-AccHolder <- numeric()
-
-for (i in 1:k){
-  # remove rows with id i from dataframe to create training set
-  trainingset <- subset(dataOW, id %in% list[-i])
-  # select rows with id i to create test set
-  testset <- subset(dataOW, id %in% c(i))
-
-  model.svm <- svm(Code ~ brightness_median + brightness_stdev + saturation_median + saturation_stdev + hue_median + hue_stdev, 
-  	kernel = "linear", type = "C-classification", data=trainingset, cost=1, gamma=0.1)
-  
-  model.svm.test <- predict(model.svm, testset[3:8])
-
-  svmResults <- cut(model.svm.test, breaks=c(-Inf, 0.5, Inf), labels=c(0, 1))
-
-  resultTable <- table(pred = svmResults, true = testset$Code)
-  Acc <- (resultTable[1,1]+resultTable[2,2])/nrow(testset)
-  AccHolder <- c(AccHolder, Acc) 
-
-}
-
-SVM.AccHolder <- AccHolder
-SVM.accuracy <- mean(SVM.AccHolder)
-SVM.accuracy
 
 
 
 
 
 ## Perennial Ice/Snow - Class12
-dataIC <- data[data$Code == "Class12", ]
+dataIC <- data[data$Code == "Class12", 1:8]
+dataIC$Code <- "Class12"
 otherIC <- data[data$Code != "Class12", 1:8]
 otherIC$Code <- "Other"
 dataIC <- rbind(dataIC, otherIC)
 
+# Training data
+TrainingDataIC <- dataIC[1:as.integer(nrow(dataIC[dataIC$Code == "Class12", ]) * 0.80), ]
+TrainingDataICOther <- dataIC[nrow(dataIC[dataIC$Code == "Class12", ])+1:as.integer(nrow(dataIC[dataIC$Code == "Other", ]) * 0.80), ]
+Trainingset <- rbind(TrainingDataIC, TrainingDataICOther)
+
+# Testing data
+TestStartIC <- nrow(TrainingDataIC)+1 
+TestFinishIC <- nrow(dataIC[dataIC$Code == "Class12", ])
+TestingDataIC <- dataIC[TestStartIC:TestFinishIC, ]
+TestStartOther <- nrow(dataIC[dataIC$Code == "Class12", ])+nrow(TrainingDataICOther)+1
+TestFinishOther <- nrow(dataIC)
+TestingDataOther <- dataIC[TestStartOther:TestFinishOther, ]
+Testingset <- rbind(TestingDataIC, TestingDataOther)
+
+# SVM 
+ICsvm <- svm(Code ~ brightness_median + brightness_stdev + saturation_median + saturation_stdev + hue_median + hue_stdev, 
+  kernel = "linear", type = "C-classification", data=Trainingset)
+summary(ICsvm)
+model.svm.test <- predict(ICsvm, Testingset[3:8], type="class")
+summary(model.svm.test)
+
+
+
+
 
 
 ## Developed, Open Space - Class21
-dataDO <- data[data$Code == "Class21", ]
+dataDO <- data[data$Code == "Class21", 1:8]
+dataDO$Code <- "Class21"
 otherDO <- data[data$Code != "Class21", 1:8]
 otherDO$Code <- "Other"
 dataDO <- rbind(dataDO, otherDO)
+
+# Training data
+TrainingDataDO <- dataDO[1:as.integer(nrow(dataDO[dataDO$Code == "Class21", ]) * 0.80), ]
+TrainingDataDOOther <- dataDO[nrow(dataDO[dataDO$Code == "Class21", ])+1:as.integer(nrow(dataDO[dataDO$Code == "Other", ]) * 0.80), ]
+Trainingset <- rbind(TrainingDataDO, TrainingDataDOOther)
+
+# Testing data
+TestStartDO <- nrow(TrainingDataDO)+1 
+TestFinishDO <- nrow(dataDO[dataDO$Code == "Class21", ])
+TestingDataDO <- dataDO[TestStartDO:TestFinishDO, ]
+TestStartOther <- nrow(dataDO[dataDO$Code == "Class21", ])+nrow(TrainingDataDOOther)+1
+TestFinishOther <- nrow(dataDO)
+TestingDataOther <- dataDO[TestStartOther:TestFinishOther, ]
+Testingset <- rbind(TestingDataDO, TestingDataOther)
+
+# SVM 
+DOsvm <- svm(Code ~ brightness_median + brightness_stdev + saturation_median + saturation_stdev + hue_median + hue_stdev, 
+  kernel = "linear", type = "C-classification", data=Trainingset)
+summary(DOsvm)
+model.svm.test <- predict(DOsvm, Testingset[3:8], type="class")
+summary(model.svm.test)
+
+
+
+
 
 
 
 
 ## Developed, Low Intensity - Class22
-dataDL <- data[data$Code == "Class22", ]
+dataDL <- data[data$Code == "Class22", 1:8]
+dataDL$Code <- "Class22"
 otherDL <- data[data$Code != "Class22", 1:8]
 otherDL$Code <- "Other"
 dataDL <- rbind(dataDL, otherDL)
+
+# Training data
+TrainingDataDL <- dataDL[1:as.integer(nrow(dataDL[dataDL$Code == "Class22", ]) * 0.80), ]
+TrainingDataDLOther <- dataDL[nrow(dataDL[dataDL$Code == "Class22", ])+1:as.integer(nrow(dataDL[dataDL$Code == "Other", ]) * 0.80), ]
+Trainingset <- rbind(TrainingDataDL, TrainingDataDLOther)
+
+# Testing data
+TestStartDL <- nrow(TrainingDataDL)+1 
+TestFinishDL <- nrow(dataDL[dataDL$Code == "Class22", ])
+TestingDataDL <- dataDL[TestStartDL:TestFinishDL, ]
+TestStartOther <- nrow(dataDL[dataDL$Code == "Class22", ])+nrow(TrainingDataDLOther)+1
+TestFinishOther <- nrow(dataDL)
+TestingDataOther <- dataDL[TestStartOther:TestFinishOther, ]
+Testingset <- rbind(TestingDataDL, TestingDataOther)
+
+# SVM 
+DLsvm <- svm(Code ~ brightness_median + brightness_stdev + saturation_median + saturation_stdev + hue_median + hue_stdev, 
+  kernel = "linear", type = "C-classification", data=Trainingset)
+summary(DLsvm)
+model.svm.test <- predict(DLsvm, Testingset[3:8], type="class")
+summary(model.svm.test)
+
+
+
+
+
+
+
+
+
 
 
 
@@ -215,20 +268,66 @@ dataEW <- rbind(dataEW, otherEW)
 
 
 
-# set.seed(022105)
-# datatr<-sample(1:1243, 993)
-data.svm0 <- svm(Code ~ brightness_median + brightness_stdev + saturation_median + saturation_stdev + hue_median + hue_stdev, cross = 5, kernel = "linear", type = "C-classification", data=data[datatr,])
-summary(data.svm0)
-table(data$Method_Code[-datatr], predict(data.svm0, data[-datatr,]))
-Pred3 <- predict(data.svm0, data[-datatr,], type = "class")
-confusionMatrix(Pred3, data$Code[-datatr])
-data.svm0.tune<-tune(svm, Code ~ brightness_median + brightness_stdev + saturation_median + saturation_stdev + hue_median + hue_stdev, data=data[datatr,], kernel = "linear", type = "C-classification",
-                     ranges=list(gamma=2^(-4:4), cost=2^(-4:4)),
-                     control = tune.control(sampling="cross", cross=5))
-summary(data.svm0.tune)
+# # set.seed(022105)
+# # datatr<-sample(1:1243, 993)
+# data.svm0 <- svm(Code ~ brightness_median + brightness_stdev + saturation_median + saturation_stdev + hue_median + hue_stdev, cross = 5, kernel = "linear", type = "C-classification", data=data[datatr,])
+# summary(data.svm0)
+# table(data$Method_Code[-datatr], predict(data.svm0, data[-datatr,]))
+# Pred3 <- predict(data.svm0, data[-datatr,], type = "class")
+# confusionMatrix(Pred3, data$Code[-datatr])
+# data.svm0.tune<-tune(svm, Code ~ brightness_median + brightness_stdev + saturation_median + saturation_stdev + hue_median + hue_stdev, data=data[datatr,], kernel = "linear", type = "C-classification",
+#                      ranges=list(gamma=2^(-4:4), cost=2^(-4:4)),
+#                      control = tune.control(sampling="cross", cross=5))
+# summary(data.svm0.tune)
 
-data.svm0.TUNED <- svm(data$Code ~ brightness_median + brightness_stdev + saturation_median + saturation_stdev + hue_median + hue_stdev, cross = 5, data=data, kernel = "linear", type = "C-classification", cost=0.0625)
-summary(data.svm0.TUNED)
-Pred4 <- predict(data.svm0.TUNED, data[-datatr,], type = "class")
-confusionMatrix(Pred4, data$Code[-datatr])
+# data.svm0.TUNED <- svm(data$Code ~ brightness_median + brightness_stdev + saturation_median + saturation_stdev + hue_median + hue_stdev, cross = 5, data=data, kernel = "linear", type = "C-classification", cost=0.0625)
+# summary(data.svm0.TUNED)
+# Pred4 <- predict(data.svm0.TUNED, data[-datatr,], type = "class")
+# confusionMatrix(Pred4, data$Code[-datatr])
 
+
+
+
+
+
+
+
+dataDO <- data[data$Code == "Class21", 1:8]
+dataDO$Code <- "Class20"
+dataDL <- data[data$Code == "Class22", 1:8]
+dataDL$Code <- "Class20"
+dataDM <- data[data$Code == "Class23", 1:8]
+dataDM$Code <- "Class20"
+dataDH <- data[data$Code == "Class24", 1:8]
+dataDH$Code <- "Class20"
+developed <- rbind(dataDO, dataDL, dataDM, dataDH)
+
+otherD <- data[data$Code != "Class21" & data$Code != "Class22" & data$Code != "Class23" & data$Code != "Class24", 1:8]
+otherD$Code <- "Other"
+
+dataDeveloped <- rbind(developed, otherD)
+
+# Training data
+TrainingDataD <- dataDeveloped[1:as.integer(nrow(dataDeveloped[dataDeveloped$Code == "Class20", ]) * 0.80), ]
+TrainingDataDOther <- dataDeveloped[nrow(dataDeveloped[dataDeveloped$Code == "Class20", ])+1:as.integer(nrow(dataDeveloped[dataDeveloped$Code == "Other", ]) * 0.80), ]
+Trainingset <- rbind(TrainingDataD, TrainingDataDOther)
+
+# Testing data
+TestStartD <- nrow(TrainingDataD)+1 
+TestFinishD <- nrow(dataDeveloped[dataDeveloped$Code == "Class20", ])
+TestingDataD <- dataDeveloped[TestStartD:TestFinishD, ]
+TestStartOther <- nrow(dataDeveloped[dataDeveloped$Code == "Class20", ])+nrow(TrainingDataDOther)+1
+TestFinishOther <- nrow(dataDeveloped)
+TestingDataOther <- dataDeveloped[TestStartOther:TestFinishOther, ]
+Testingset <- rbind(TestingDataD, TestingDataOther)
+
+# SVM 
+Dsvm <- svm(Code ~ brightness_median + brightness_stdev + saturation_median + saturation_stdev + hue_median + hue_stdev, 
+  kernel = "linear", type = "C-classification", data=Trainingset)
+summary(Dsvm)
+model.svm.test <- predict(Dsvm, Testingset[3:8], type="class")
+summary(model.svm.test)
+
+
+## Summary, still placing all images in 'developed' and none in other. Developed accounts for ~75% of the data
+## It seems the visual signitures aren't doing so well. 
